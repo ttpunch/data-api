d1306f1452e8c0c5f5196bc9c819a63061189695
ttpunch <vinod418@gmail.com>
2022-12-07 23:24:24 +0530

    data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const machine = require("./models/machine.js");
 const express = require("express");
+var cors = require('cors')
 const app = express();
 const dotenv=require('dotenv')
 
@@ -16,13 +17,18 @@ const editFormRoute=require('./Router/editFormRoute')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
  
+var corsOptions = {
+  origin: 'https://data-api-d6lk.onrender.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 //Submit form Data 
-app.use('/machinedata',bgroute)
-app.use('/submit-form',formroute)
-app.use('/machineroute',searchroute)
-app.use('/editdata/',editFormRoute)
+app.use('/machinedata',cors(corsOptions),bgroute)
+app.use('/submit-form',cors(corsOptions),formroute)
+app.use('/machineroute',cors(corsOptions),searchroute)
+app.use('/editdata/',cors(corsOptions),editFormRoute)
 //________________________________________________________________________________________________________________________________
 
 const dbconnect = async () => {
