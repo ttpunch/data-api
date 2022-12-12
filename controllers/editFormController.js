2fa6691f3a48b0fb92545bdd845c009fe40799bb
ttpunch <vinod418@gmail.com>
2022-12-12 23:49:55 +0530

    .

--- controllers/editFormController.js
+++ controllers/editFormController.js
@@ -3,14 +3,17 @@ const machine = require("../models/machine.js");
 const EditformController = async (req, res) => {
   const { id } = req.params;
 
-   try {
-        await machine.findByIdAndUpdate(id, {
-        breakdown: req.body.breakdown,
-       });res.status(201).send(data);
-      } catch (e) {
-        res.status(400).send(e.message);
-      }
- 
+  try {
+    await machine.findByIdAndUpdate(id, {
+      breakdown: req.body.breakdown,
+    },
+    {
+      new: true
+    });
+    res.status(201).send(data);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 };
 
 module.exports = EditformController;
